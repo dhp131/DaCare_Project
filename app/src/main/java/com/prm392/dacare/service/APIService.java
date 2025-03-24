b30e7d1cdb53ea9c9f76baf5ae6006bf2905d38b
ThoNT16 <truongthonguyen107@gmail.com>
2025-03-24 15:32:07 +0700

    feat: config interceptor [thont]

--- app/src/main/java/com/prm392/dacare/service/APIService.java
+++ app/src/main/java/com/prm392/dacare/service/APIService.java
@@ -9,6 +9,10 @@ import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface APIService {
+
+    //Auth
     @POST("auth/login")
     Call<LoginResponse> login(@Body LoginRequest request);
+
+    //Product
 }
