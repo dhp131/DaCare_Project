31d46854ce2c9f404fb85b291bb979d22a365350
ThoNT16 <truongthonguyen107@gmail.com>
2025-03-24 15:55:09 +0700

    feat: user fragment with log out acction [thont]

---
+++ app/src/main/java/com/prm392/dacare/ui/user/UserFragment.java
@@ -0,0 +1,54 @@
+package com.prm392.dacare.ui.user;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.prm392.dacare.R;
+import com.prm392.dacare.ui.LoginActivity;
+import com.prm392.dacare.utils.SharedPreferencesUtil;
+
+public class UserFragment extends Fragment {
+
+    private UserViewModel mViewModel;
+    private Button btnLogout;
+
+    public static UserFragment newInstance() {
+        return new UserFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_user, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+        // TODO: Use the ViewModel
+        btnLogout = getView().findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferencesUtil.clearAccessToken();
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                getActivity().finish();
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
