897469e29d56cfaae0ad310c76c7193748f267af
ThoNT16 <truongthonguyen107@gmail.com>
2025-03-28 20:30:48 +0700

    feat: add slider image home page [thont]

--- app/src/main/java/com/prm392/dacare/ui/home/HomeFragment.java
+++ app/src/main/java/com/prm392/dacare/ui/home/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.prm392.dacare.ui.home;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,10 +16,15 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.prm392.dacare.R;
+import com.prm392.dacare.adapter.ImageSliderAdapter;
 import com.prm392.dacare.adapter.ProductPaginationAdapter;
 import com.prm392.dacare.databinding.FragmentHomeBinding;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
@@ -29,11 +36,19 @@ public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
 
 
+    private ViewPager2 viewPager;
+    private ImageSliderAdapter sliderAdapter;
+    private Handler handler = new Handler(Looper.getMainLooper());
+    private Runnable runnable;
+    private int currentPage = 0;
+    private final List<Integer> imageList = Arrays.asList(
+            R.drawable.slider1, R.drawable.slider2, R.drawable.slider3, R.drawable.slider4
+    );
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        viewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        viewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
@@ -56,16 +71,25 @@ public class HomeFragment extends Fragment {
             }
 
         });
-        adapter.notifyDataSetChanged();
 
-
+        viewPager = root.findViewById(R.id.viewPagerSlider);
+        sliderAdapter = new ImageSliderAdapter(imageList);
+        viewPager.setAdapter(sliderAdapter);
+        runnable = () -> {
+            if (currentPage == imageList.size()) {
+                currentPage = 0;
+            }
+            viewPager.setCurrentItem(currentPage++, true);
+            handler.postDelayed(runnable, 3000);
+        };
+        handler.postDelayed(runnable, 3000);
         return root;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        handler.removeCallbacks(runnable);
         binding = null;
     }
-
 }
